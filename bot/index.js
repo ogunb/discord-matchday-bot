099265512b76fef416aea6446d17e13ec5a49c52
Ogün Babacan <ogun.babacan@trendyol.com>
2021-03-09 21:24:28 +0300

    feat(twitter): add twitter service and fetch match url

--- bot/index.js
+++ bot/index.js
@@ -1,13 +1,17 @@
 const DiscordService = require('./services/DiscordService');
+const TwitterService = require('./services/TwitterService');
 
-function sendMatchMessage(req, res) {
+function sendMatchMessage(event, context) {
   DiscordService.login().then(() => {
-    const channel = DiscordService.findChannel((channel) => {
-      return channel.name === process.env.CHANNEL_NAME
-    });
+    TwitterService.fetchTotallyLegitMatchUrl().then(matchUrl => {
+      const data = JSON.parse(Buffer.from(event.data, 'base64').toString());
 
-    DiscordService.sendMessage(channel, 'sa');
-    res.send("OK")
+      const channel = DiscordService.findChannel((channel) => {
+        return channel.name === process.env.CHANNEL_NAME
+      });
+
+      DiscordService.sendMessage(channel, `${data.event} - ${matchUrl}`);
+    })
   });
 };
 
