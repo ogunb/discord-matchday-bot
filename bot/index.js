d492cafb94140bbb930f0cb1f1b3aaa948c83ceb
Ogun Babacan <babacanogun@gmail.com>
2021-08-26 16:05:47 +0000

    feat: remove match url fetch

--- bot/index.js
+++ bot/index.js
@@ -3,15 +3,13 @@ const TwitterService = require('./services/TwitterService');
 
 function sendMatchMessage(event, context) {
   DiscordService.login().then(() => {
-    TwitterService.fetchTotallyLegitMatchUrl().then(matchUrl => {
-      const data = JSON.parse(Buffer.from(event.data, 'base64').toString());
+    const data = JSON.parse(Buffer.from(event.data, 'base64').toString());
 
-      const channel = DiscordService.findChannel((channel) => {
-        return channel.name === process.env.CHANNEL_NAME
-      });
+    const channel = DiscordService.findChannel((channel) => {
+      return channel.name === process.env.CHANNEL_NAME
+    });
 
-      DiscordService.sendMessage(channel, `${data.event} - ${matchUrl}`);
-    })
+    DiscordService.sendMessage(channel, `${data.event}`);
   });
 };
 
